f8bac6d274f334aa7d6656c2b78f61e23f35f760
KosherBacon <j.harris.kahn@gmail.com>
2018-06-20 14:46:17 -0700

    Setup docker-entrypoint to run navajoanonsubchaind.

--- subchain/docker-entrypoint.sh
+++ subchain/docker-entrypoint.sh
@@ -1,2 +1,26 @@
 #!/bin/sh
 set -ex
+
+if [ $(echo "$1" | cut -c1) = "-" ]; then
+  echo "$0: assuming arguments for navajoanonsubchaind"
+
+  set -- navcoind "$@"
+fi
+
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "navajoanonsubchaind" ]; then
+  mkdir -p "$SUBCHAIN_DATA"
+  chmod 700 "$SUBCHAIN_DATA"
+  chown -R subchain "$SUBCHAIN_DATA"
+
+  echo "$0: setting data directory to $SUBCHAIN_DATA"
+
+  set -- "$@" -datadir="$SUBCHAIN_DATA"
+fi
+
+if [ "$1" = "navajoanonsubchaind" ]]; then
+  echo
+  exec su-exec subchain "$@"
+fi
+
+echo
+exec "$@"
