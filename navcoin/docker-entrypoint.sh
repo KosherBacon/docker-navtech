32900cff323f762651e82a8158fff5d3b69db674
KosherBacon <j.harris.kahn@gmail.com>
2018-06-20 14:42:42 -0700

    Setup docker-entrypoint to run navcoind.

--- navcoin/docker-entrypoint.sh
+++ navcoin/docker-entrypoint.sh
@@ -1,2 +1,26 @@
 #!/bin/sh
 set -ex
+
+if [ $(echo "$1" | cut -c1) = "-" ]; then
+  echo "$0: assuming arguments for navcoind"
+
+  set -- navcoind "$@"
+fi
+
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "navcoind" ]; then
+  mkdir -p "$NAVCOIN_DATA"
+  chmod 700 "$NAVCOIN_DATA"
+  chown -R navcoin "$NAVCOIN_DATA"
+
+  echo "$0: setting data directory to $NAVCOIN_DATA"
+
+  set -- "$@" -datadir="$NAVCOIN_DATA"
+fi
+
+if [ "$1" = "navcoind" ] || [ "$1" = "navcoin-cli" ] || [ "$1" = "navcoin-tx" ]; then
+  echo
+  exec su-exec navcoin "$@"
+fi
+
+echo
+exec "$@"
